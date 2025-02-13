3f2a2339e2de84cbac8f32d87cd834625a306832
xrt19 <irfanhera17@gmail.com>
2025-02-13 09:19:42 +0700

    -

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,10 @@
+// import "dotenv/config";
+
+// export const PORT = 5555;
+
+// export const mongoDBURL = process.env.mongoDBURL;
+
 import "dotenv/config";
 
-export const PORT = 5555;
-
-export const mongoDBURL = process.env.mongoDBURL;
+export const PORT = process.env.PORT || 5555;
+export const mongoDBURL = process.env.MONGODB_URI;
